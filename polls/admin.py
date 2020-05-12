bef03fd4be3b294726580fbb756a6e40234b1bd9
shaffat <shaffatbd@gmail.com>
2020-05-12 23:13:51 +0600

    customize admin template

--- polls/admin.py
+++ polls/admin.py
@@ -11,9 +11,11 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
+    search_fields = ['question_text']
     fieldsets = [
         (None,               {'fields':['question_text']}),
         ('Date information', {'fields':['pub_date'], 'classes':['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 admin.site.register(Question,QuestionAdmin)
